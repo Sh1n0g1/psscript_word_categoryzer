0b41a2c7e95cb4aadb01078b3059fe78f13b62b6
Sh1n0g1 <ryonoryo@gmail.com>
2022-08-16 10:33:51 +0900

    revise statistics

--- gen_statistics.py
+++ gen_statistics.py
@@ -1,18 +1,31 @@
 import os
+import sys
 import csv
 import json
 alldata={}
-for curdir,dirs,files in os.walk("output"):
-  for file in files:
-    print(os.path.join(curdir, file))
-    with open(os.path.join(curdir, file), encoding='utf-8') as f:
-      reader=csv.reader(f)
-      for row in reader:
-        keyword=row[0]
-        occurrence=row[1]
-        if keyword in alldata:
-          alldata[keyword]+=1
-        else:
-          alldata[keyword]=1
-alldata={word: rank for word, rank in sorted(alldata.items(), key=lambda item: item[1], reverse=True)}
-print(json.dumps(alldata))
+
+def sum_ranking(dirpath, outputfile):
+  for curdir,dirs,files in os.walk(dirpath):
+    for file in files:
+      print(os.path.join(curdir, file))
+      with open(os.path.join(curdir, file), encoding='utf-8') as f:
+        reader=csv.reader(f)
+        for row in reader:
+          keyword=row[0]
+          occurrence=row[1]
+          if keyword in alldata:
+            alldata[keyword]+=1
+          else:
+            alldata[keyword]=1
+  alldata={word: rank for word, rank in sorted(alldata.items(), key=lambda item: item[1], reverse=True)}
+  with open(outputfile, 'w') as f:
+    json.dump(alldata,f)
+  print(json.dumps(alldata))
+
+if __name__=='__main__':
+  if len(sys.argv) < 3:
+    print('Usage: python gen_ranking_file <dirpath> <outputfile>')
+    exit()
+  dirpath=sys.argv[1]
+  outputfile=sys.argv[2]
+  sum_ranking(dirpath, outputfile)
